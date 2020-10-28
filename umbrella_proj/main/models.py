b78c4fb4bf62758a2b297e3a7b9daec5ead41b26
manfromsouth <igor.bordonos@gmail.com>
2020-10-28 17:20:59 +0300

    Fifth commit: fixed issue with apps, not properly loading, apps moved inside project directory, migrations properly made this time and done

---
+++ umbrella_proj/main/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+# Здесь хранится общая информация о заказах
+class Orders(models.Model):
+    # Выборы для choices
+    FULFILLED = 'F'  # заказ оплачен и получен
+    CANCELED = 'C'  # заказ оплачен и отменен
+    PAYED = 'P'  # заказ оплачен и не получен
+    RESERVED = 'R'  # заказ как либо зарезервирован
+    OPENED = 'O'  # заказ открыт и неоплачен (в корзине)
+    # ХЗ как сделать так, чтобы можно было прочитать, но не изменить эти поля
+    status_choices = [(FULFILLED, 'Получен'), (CANCELED, 'Отменен'), (PAYED, 'Оплачен'),
+                      (RESERVED, 'Зарезервирован'), (OPENED, 'Открыт')]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # TODO: Проработать choices
+    payment_type = models.CharField(max_length=2)
+    # TODO: в тестах проверять наличие всех адрессов для данного пользователя в таблице UserAddresses
+    order_address = models.CharField(max_length=512)
+    # TODO: в тестах проверять наличие всех адрессов для данного пользователя в таблице UserPhones
+    order_phone_num = PhoneNumberField()
+    order_status = models.CharField(max_length=1, choices=status_choices)
+    # поле вычисляется на основе OrderGoods
+    order_cost = models.FloatField()
+    # здесь проставливаются даты установления соответствующих состояний заказов
+    date_reserved = models.DateField()
+    date_payed = models.DateField()
+    date_fulfilled = models.DateField()
+
+
+class Goods(models.Model):
+    PERFUME = 'per'
+    COSMETICS = 'cos'
+    goods_types = [(PERFUME, 'Парфюм'), (COSMETICS, 'Косметика')]
+    name = models.CharField(max_length=256)
+    price = models.FloatField()
+    producer = models.CharField(max_length=256)
+    info = models.CharField(max_length=512)  # отображаемая инфа о товаре
+    image = models.ImageField()  # большой вариант катринки товара
+    image_preview = models.ImageField()  # малый вариант картинки товара, используемый в спискахэ
+    amount = models.IntegerField()  # количество товара на "складе"
+    type = models.CharField(max_length=3, choices=goods_types)  # типы для поиска
+    show = models.BooleanField()
+
+
+# доп инфа для парфюма
+class Perfume(models.Model):
+    good = models.OneToOneField(Goods, on_delete=models.CASCADE)
+    aroma = models.CharField(max_length=256)
+
+
+class SpecialOffers(models.Model):
+    DISCOUNT = 'dis'
+    offer_types = [(DISCOUNT, 'Скидка')]
+    date_begin = models.DateField()  # Начало акции
+    date_ends = models.DateField()  # Конец акции
+    offer_image = models.ImageField()  # Изображение, показуемое на экране
+    type = models.CharField(max_length=3, choices=offer_types)
+
+
+class Discounts(models.Model):
+    offer = models.ForeignKey(SpecialOffers, on_delete=models.CASCADE)
+    good = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    amount = models.IntegerField()  # измеряется в процентах от оригинальной цены
+
+
+
--- umbrella_proj/umbrella_proj/urls.py
+++ umbrella_proj/umbrella_proj/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from cart import views
+# from main import views
+# from stats import views
+# from user_control import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
